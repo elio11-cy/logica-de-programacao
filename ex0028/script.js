cfcc1a7e8a1d4de696b9e1a79c0b4d22da219afa
Kayck otavio <157185739+thislord13@users.noreply.github.com>
2026-06-02 20:54:28 -0300

    atv
    Java script

---
+++ ex0028/script.js
@@ -0,0 +1,10 @@
+let cadastro  = [
+    ["Jhonata", 17, "91kg", 1.83, ['Iza', 'Preto']],
+    ['Paulo', 18, '80', 1.75, ['Dozinha', 'Mingua']]
+]
+for(let i = 0; i<=cadastro.length-1;i++){
+    for(let y = 0; y<= cadastro[0][4].length - 1; y++){
+        console.log(cadastro[i][0] - cadastro[i][4][y])
+    }
+}
+   
