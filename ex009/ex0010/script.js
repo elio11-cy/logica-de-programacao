8fb7a86a26467887b5421cf1925e51a48dbf31b9
Kayck otavio <157185739+thislord13@users.noreply.github.com>
2026-06-03 21:25:15 -0300

    Atv passadas

--- ex009/ex0010/script.js
+++ ex009/ex0010/script.js
@@ -1,12 +1,8 @@
-var titulo = document.getElementById
-('titulo')
-var paragrago = document.getElementById
-('paragrafo')
-var div1 = document.getElementById
-('div1')
-var div2 = document.getElementById
-('div2')
-console.log(titulo)
+var titulo = document.getElementById('titulo');
+var paragrago = document.getElementById('paragrafo');
+var div1 = document.getElementById('div1');
+var div2 = document.getElementById('div2');
+console.log(titulo);
 
-titulo.style.color = 'red'
-div1.innerHTML = `Olá mundo`
+titulo.style.color = 'red';
+div1.innerHTML = `Olá mundo`;
--- ex013/script.js
+++ ex013/script.js
@@ -1,22 +1,25 @@
-function  somar(){
-    let num1 = Numer(document.getElementById("aNum1").value)
-    let num2 = Numer(document.getElementById("aNumer2").value)
-    console.log("SOMAR")
-    console.log(num1)
-    console.log(num2)
-    console.log(typeof(num1))
-    console.log(typeof(num2))
+function somar() {
+    let num1 = Number(document.getElementById("aNum1").value);
+    let num2 = Number(document.getElementById("aNum2").value);
+    const resultado = num1 + num2;
+    document.getElementById("sResultado").innerText = `Resultado da soma: ${resultado}`;
+    console.log("SOMAR");
+    console.log(num1);
+    console.log(num2);
+    console.log(typeof num1);
+    console.log(typeof num2);
 }
 
-function subtrair(){
-    let num1 = Numer(document.getElementById("sNumer1").value)
-    let num2 = Numer(document.getElementById("sNumer2").value)
-    console.log("SUBTRAIR")
-    console.log("num1")
-    console.log("num2")
-    console.log(typeof(num1))
-    console.log(typeof(num2))
-
+function subtrair() {
+    let num1 = Number(document.getElementById("sNum1").value);
+    let num2 = Number(document.getElementById("sNum2").value);
+    const resultado = num1 - num2;
+    document.getElementById("sResultado").innerText = `Resultado da subtração: ${resultado}`;
+    console.log("SUBTRAIR");
+    console.log(num1);
+    console.log(num2);
+    console.log(typeof num1);
+    console.log(typeof num2);
 }
 
 
